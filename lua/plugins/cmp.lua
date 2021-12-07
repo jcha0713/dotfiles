3f6b25ad57d5742fbe1cbc5bc2519762abdffb4e
jcha0713 <joocha0713@gmail.com>
2021-12-06 19:43:01 -0800

    minor changes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,72 +1,66 @@
-local cmp = require 'cmp'
-local lspkind = require 'lspkind'
+local cmp = require "cmp"
+
+local lspkind = require "lspkind"
 
 cmp.setup {
-  documentation = {
-    border = 'double',
-  },
   snippet = {
     expand = function(args)
-      require'luasnip'.lsp_expand(args.body)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
   formatting = {
-    format = lspkind.cmp_format({
+    format = lspkind.cmp_format {
       with_text = true,
       maxwidth = math.floor(vim.api.nvim_win_get_width(0) / 2),
       maxheight = math.floor(vim.api.nvim_win_get_height(0) / 3 * 2),
-      menu = ({
-        buffer = '[buf]',
-        nvim_lsp = '[lsp]',
-        luasnip = '[snip]',
-        nvim_lua = '[lua]',
-        latex_symbols = '[latex]',
-      }),
-    }),
+      menu = {
+        buffer = "[Buf]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snip]",
+        nvim_lua = "[Lua]",
+        cmp_tabnine = "[TN]",
+      },
+    },
   },
   mapping = {
-    ['<c-p>'] = cmp.mapping.select_prev_item(),
-    ['<c-n>'] = cmp.mapping.select_next_item(),
-    ['<c-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<c-u>'] = cmp.mapping.scroll_docs(4),
-    ['<c-space>'] = cmp.mapping.complete(),
-    ['<c-e>'] = cmp.mapping.close(),
-    ['<c-k>'] = cmp.mapping.confirm {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(4),
+    ["<C-space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm(),
+    --[[ ["<C-f>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
-    ['<tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<c-k>', true, true, true))
-      -- elseif require'luasnip'.expand_or_jumpable() then
-        -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<plug>luasnip-expand-or-jump', true, true, true), '')
-      else
-        fallback()
-      end
-    end,
-    ['<s-tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<c-p>', true, true, true), 'n')
-      -- elseif require'luasnip'.jumpable(-1) then
-        -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<plug>luasnip-jump-prev', true, true, true), '')
-      else
-        fallback()
-      end
-    end,
+    }, ]]
+    --   ["<tab>"] = function(fallback)
+    --     if
+    --     else
+    --       fallback()
+    --     end
+    --   end,
+    --   ["<s-tab>"] = function(fallback)
+    --     if
+    --     else
+    --       fallback()
+    --     end
+    --   end,
   },
   sources = {
-    -- { name = 'nvim_lua' },
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'luasnip' },
+    { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "cmp_tabnine" },
+    { name = "path" },
+    { name = "luasnip" },
     {
-      name = 'fuzzy_buffer',
-      keyword_length = 4,
-      max_item_count = 12,
+      name = "fuzzy_buffer",
+      keyword_length = 5,
+      max_item_count = 10,
     },
   },
   experimental = {
     native_menu = false,
     ghost_text = false,
-  }
+  },
 }
--- lua/plugins/textobjects.lua
+++ lua/plugins/textobjects.lua
@@ -1,9 +1,9 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup {
   textobjects = {
     select = {
       enable = true,
 
-      -- Automatically jump forward to textobj, similar to targets.vim 
+      -- Automatically jump forward to textobj, similar to targets.vim
       lookahead = true,
 
       keymaps = {
