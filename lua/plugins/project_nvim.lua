315370860ae9c89f5b50978c5cc451b9965bbebb
jcha0713 <joocha0713@gmail.com>
2021-12-17 15:15:20 -0800

    change detect pattern

--- lua/plugins/project_nvim.lua
+++ lua/plugins/project_nvim.lua
@@ -1,4 +1,6 @@
 require("project_nvim").setup {
-  exclude_dirs = { '~/.config/*' },
-    show_hidden = false,
+  exclude_dirs = { "~/.config/*" },
+  show_hidden = false,
+  detection_method = { "lsp", "pattern" },
+  patterns = { ".git", ".package.json", "stylua.toml", "tsconfig.json" },
 }
