e6b76d6bc848c534da9b4e2aac11c680c1bdb04d
jcha0713 <joocha0713@gmail.com>
2021-12-17 12:37:37 -0800

    quotes

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 -- vim.o.runtimepath = vim.o.runtimepath..',/Users/jcha0713/.config/nvim/lua/configs/friendly-snippets,'
 
 -- General settings
-require('plugins')
-require('modules.options')
-require('modules.keymappings')
-require('modules.utils')
-require('modules.autocmd')
-
-
-
+require "plugins"
+require "modules.options"
+require "modules.keymappings"
+require "modules.utils"
+require "modules.autocmd"
--- lua/modules/keymappings.lua
+++ lua/modules/keymappings.lua
@@ -1,72 +1,83 @@
-local utils = require('modules.utils')
+local utils = require "modules.utils"
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- full help page
-utils.map('c', ':help', ':tab help<CR>')
+utils.map("c", ":help", ":tab help<CR>")
 
 -- easy keys for ESC
-utils.map('i', 'jj', '<ESC>')
+utils.map("i", "jj", "<ESC>")
+
+-- change colorscheme to gruvbox
+utils.map("n", "<Leader>cc", ":colorscheme gruvbox<CR>")
+
+-- split windows
+utils.map("n", "<Leader>|", "<C-W>v")
+utils.map("n", "<Leader>_", "<C-W>s")
+
+-- fzf-dictionary
+vim.cmd [[  imap <C-l> <Plug>(fzf-dictionary-open) ]]
 
 -- paste with Enter key
-utils.map('n', '<CR>', 'o<ESC>k')
+utils.map("n", "<CR>", "o<ESC>k")
 
 -- toggle hl
 -- utils.map('n', '<Leader>h', ':set hlsearch!<CR>')
 
 -- explorer
-utils.map('n', '<C-n>', ':NvimTreeToggle<CR>')
+utils.map("n", "<C-n>", ":NvimTreeToggle<CR>")
 
 -- Enter Goyo
-utils.map('n', '<Leader>g', ':Goyo<CR>')
+utils.map("n", "<Leader>g", ":Goyo<CR>")
 
 -- source lua file
-utils.map('n', '<Leader>s', ':luafile %<CR>')
+utils.map("n", "<Leader>s", ":luafile %<CR>")
 
 -- better window movement
-utils.map('n', '<C-h>', '<C-w>h')
-utils.map('n', '<C-j>', '<C-w>j')
-utils.map('n', '<C-k>', '<C-w>k')
-utils.map('n', '<C-l>', '<C-w>l')
+utils.map("n", "<C-h>", "<C-w>h")
+utils.map("n", "<C-j>", "<C-w>j")
+utils.map("n", "<C-k>", "<C-w>k")
+utils.map("n", "<C-l>", "<C-w>l")
 
 -- Switch buffers
-utils.map('n', '<S-k>', ':BufferLineCycleNext<CR>')
-utils.map('n', '<S-j>', ':BufferLineCyclePrev<CR>')
-utils.map('n', '<Leader>x', ':bdelete<CR>')
+utils.map("n", "<S-k>", ":BufferLineCycleNext<CR>")
+utils.map("n", "<S-j>", ":BufferLineCyclePrev<CR>")
+utils.map("n", "<Leader>x", ":bdelete<CR>")
 
 -- Rainbow Parentheses
-utils.map('n', '<Leader>p', ':RainbowParentheses!!<CR>')
+utils.map("n", "<Leader>p", ":RainbowParentheses!!<CR>")
 
 -- Clear highlight
-utils.map('n', '<Leader>n', '<cmd>noh<CR>')
+utils.map("n", "<Leader>n", "<cmd>noh<CR>")
 
 -- Telescope
-utils.map('n', '<Leader>ff', ':Telescope find_files<CR>')
-utils.map('n', '<Leader>fg', ':Telescope live_grep<CR>')
-utils.map('n', '<Leader>fb', ':Telescope buffers<CR>')
-utils.map('n', '<Leader>fr', ':Telescope projects<CR>')
-utils.map('n', '<Leader>fh', ':Telescope help_tags<CR>')
+utils.map("n", "<Leader>ff", ":Telescope find_files<CR>")
+utils.map("n", "<Leader>fg", ":Telescope live_grep<CR>")
+utils.map("n", "<Leader>fb", ":Telescope buffers<CR>")
+utils.map("n", "<Leader>fr", ":Telescope projects<CR>")
+utils.map("n", "<Leader>fh", ":Telescope help_tags<CR>")
+utils.map("n", "<Leader>fo", ":Telescope oldfiles<CR>")
 
 -- always keep the cursor at center
-utils.map('n', 'n', 'nzzzv')
-utils.map('n', 'N', 'Nzzzv')
-utils.map('n', '<Leader>j', 'mzJ`z')
+utils.map("n", "n", "nzzzv")
+utils.map("n", "N", "Nzzzv")
+utils.map("n", "<Leader>j", "mzJ`z")
 
 -- set break points for undos
-utils.map('i', ',', ',<C-g>u')
-utils.map('i', '.', '.<C-g>u')
-utils.map('i', '<', '<<C-g>u')
-utils.map('i', '>', '><C-g>u')
-utils.map('i', '(', '(<C-g>u')
-utils.map('i', ')', ')<C-g>u')
-utils.map('i', '[', '[<C-g>u')
-utils.map('i', ']', ']<C-g>u')
+utils.map("i", ",", ",<C-g>u")
+utils.map("i", ".", ".<C-g>u")
+utils.map("i", "<", "<<C-g>u")
+utils.map("i", ">", "><C-g>u")
+utils.map("i", "(", "(<C-g>u")
+utils.map("i", ")", ")<C-g>u")
+utils.map("i", "[", "[<C-g>u")
+utils.map("i", "]", "]<C-g>u")
 
 -- move line
-utils.map('v', '<C-j>', ":m '>+1<CR>gv=gv")
-utils.map('v', '<C-k>', ":m '<-2<CR>gv=gv")
-utils.map('n', '<Leader>k', ":m .-2<CR>==")
-utils.map('n', '<Leader>j', ":m .+1<CR>==")
+utils.map("v", "<C-j>", ":m '>+1<CR>gv=gv")
+utils.map("v", "<C-k>", ":m '<-2<CR>gv=gv")
+utils.map("n", "<Leader>k", ":m .-2<CR>==")
+utils.map("n", "<Leader>j", ":m .+1<CR>==")
 
 -- vv for select line
-utils.map ('n', 'vv', '<S-v>')
+utils.map("n", "vv", "<S-v>")
