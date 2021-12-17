5c3e554f9f9ddfe05b98fc017e31976e9110d020
jcha0713 <joocha0713@gmail.com>
2021-12-17 12:47:27 -0800

    quote

--- lua/modules/utils.lua
+++ lua/modules/utils.lua
@@ -1,36 +1,55 @@
 local utils = {}
 local api = vim.api
 
-local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
 
 local get_map_options = function(custom_options)
-    local options = { noremap = true, silent = true }
-    if custom_options then
-        options = vim.tbl_extend("force", options, custom_options)
-    end
-    return options
+  local options = { noremap = true, silent = true }
+  if custom_options then
+    options = vim.tbl_extend("force", options, custom_options)
+  end
+  return options
+end
+
+local get_map_options_expr = function(custom_options)
+  local options = { expr = true, noremap = true, silent = true }
+  if custom_options then
+    options = vim.tbl_extend("force", options, custom_options)
+  end
+  return options
 end
 
 function utils.opt(scope, key, value)
-    scopes[scope][key] = value
-    if scope ~= 'o' then scopes['o'][key] = value end
+  scopes[scope][key] = value
+  if scope ~= "o" then
+    scopes["o"][key] = value
+  end
 end
 
 function utils.map(mode, lhs, rhs, opts)
   api.nvim_set_keymap(mode, lhs, rhs, get_map_options(opts))
 end
 
+function utils.map_expr(mode, lhs, rhs, opts)
+  api.nvim_set_keymap(mode, lhs, rhs, get_map_options_expr(opts))
+end
+
 utils.buf_map = function(mode, target, source, opts, bufnr)
-    api.nvim_buf_set_keymap(bufnr or 0, mode, target, source, get_map_options(opts))
+  api.nvim_buf_set_keymap(
+    bufnr or 0,
+    mode,
+    target,
+    source,
+    get_map_options(opts)
+  )
 end
 
 utils.command = function(name, fn)
-    vim.cmd(string.format("command! %s %s", name, fn))
+  vim.cmd(string.format("command! %s %s", name, fn))
 end
 
 utils.lua_command = function(name, fn)
-    utils.command(name, "lua " .. fn)
+  utils.command(name, "lua " .. fn)
 end
 
 return utils
-
