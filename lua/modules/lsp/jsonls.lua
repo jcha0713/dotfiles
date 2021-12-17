b14f839d49b6a6b9681c593d66d0655452478043
jcha0713 <joocha0713@gmail.com>
2021-12-17 12:40:09 -0800

    jsonc settings

--- lua/modules/lsp/jsonls.lua
+++ lua/modules/lsp/jsonls.lua
@@ -1,18 +1,19 @@
 local M = {}
 
 M.setup = function(on_attach)
-    require("lspconfig").jsonls.setup({
-        settings = {
-            json = {
-                schemas = require("schemastore").json.schemas(),
-            },
-        },
-        on_attach = function(client)
-            client.resolved_capabilities.document_formatting = false
+  require("lspconfig").jsonls.setup {
+    filetypes = { "json", "jsonc" },
+    settings = {
+      json = {
+        schemas = require("schemastore").json.schemas(),
+      },
+    },
+    on_attach = function(client)
+      client.resolved_capabilities.document_formatting = false
 
-            on_attach(client)
-        end,
-    })
+      on_attach(client)
+    end,
+  }
 end
 
 return M
