d73c04b35bc57aef28c0374bce7473b6395d981b
jcha0713 <joocha0713@gmail.com>
2021-12-17 12:47:52 -0800

    autopair when () is typed but commented out for future use

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,1 +1,12 @@
-require('nvim-autopairs').setup()
+local npairs = require "nvim-autopairs"
+local Rule = require "nvim-autopairs.rule"
+
+npairs.setup()
+
+--[[ npairs.add_rules {
+  Rule(
+    "%(.*%)%s*%=>$",
+    " {  }",
+    { "typescript", "typescriptreact", "javascript" }
+  ):use_regex(true):set_end_pair_length(2),
+} ]]
