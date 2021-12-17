f56119c025b46e720622d2e322306b66f43742c6
jcha0713 <joocha0713@gmail.com>
2021-12-17 15:29:11 -0800

    gj for j and gk for k in markdown files for better movement

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,14 @@
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "j",
+  "gj",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "k",
+  "gk",
+  { noremap = true, silent = true }
+)
