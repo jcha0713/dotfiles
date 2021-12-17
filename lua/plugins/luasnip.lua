b1f188735c415138bc53370a5eda9aa23b1c128d
jcha0713 <joocha0713@gmail.com>
2021-12-17 12:51:27 -0800

    add typescript snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -11,6 +11,7 @@ ls.snippets = {
 
 -- enable html snippets in javascript/javascript(REACT)
 ls.snippets.javascript = ls.snippets.html
+ls.snippets.typescript = ls.snippets.html
 ls.snippets.javascriptreact = ls.snippets.html
 ls.snippets.typescriptreact = ls.snippets.html
 require("luasnip/loaders/from_vscode").lazy_load {
