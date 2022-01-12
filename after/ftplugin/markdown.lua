d4f8853d7d1f65aa6c24b57acb6edf1b856d1aa3
jcha0713 <joocha0713@gmail.com>
2022-01-11 21:41:17 -0800

    change conceallevel, pencil mode to soft

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -12,3 +12,14 @@ vim.api.nvim_buf_set_keymap(
   "gk",
   { noremap = true, silent = true }
 )
+
+vim.opt.conceallevel = 2
+vim.cmd [[
+let g:pencil#wrapModeDefault = 'soft'
+
+augroup pencil
+  autocmd!
+  autocmd FileType markdown,mkd call pencil#init()
+  autocmd FileType text         call pencil#init()
+augroup END
+]]
