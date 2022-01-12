3b33f627b6fe1979a1498636e480f0cd7d1e4787
jcha0713 <joocha0713@gmail.com>
2022-01-11 21:45:06 -0800

    minor fix

--- lua/modules/lsp/cssls.lua
+++ lua/modules/lsp/cssls.lua
@@ -1,15 +1,22 @@
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-    lspconfig.cssls.setup({
-      on_attach = function(client)
-        on_attach(client)
-      end,
-      capabilities = capabilities,
-      cmd = { 'vscode-css-language-server', '--stdio' }
-    })
+  lspconfig.cssls.setup {
+    on_attach = function(client)
+      on_attach(client)
+    end,
+    capabilities = capabilities,
+    cmd = { "vscode-css-language-server", "--stdio" },
+    settings = {
+      css = {
+        lint = {
+          unknownAtRules = "ignore",
+        },
+      },
+    },
+  }
 end
 
 return M
