361640745fe23c52be6ee71006b51e0e7548983d
jcha0713 <joocha0713@gmail.com>
2022-01-11 21:43:00 -0800

    open help pages in full length, highlight on yan

--- lua/modules/autocmd.lua
+++ lua/modules/autocmd.lua
@@ -1,4 +1,4 @@
 local cmd = vim.cmd
 
-cmd[[au TextYankPost * lua vim.highlight.on_yank {on_visual = false}]] -- highlint on yank
-cmd[[autocmd BufEnter *.txt if &filetype == 'help' | wincmd T | endif]] -- always open help as a new tab
+cmd [[au TextYankPost * lua vim.highlight.on_yank {on_visual = false}]] -- highlint on yank
+cmd [[autocmd BufEnter *.txt if &filetype == 'help' | wincmd T | endif]] -- always open help as a new tab
