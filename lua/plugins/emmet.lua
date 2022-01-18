12e56ecc6409354701a45c9b3a11d2cc02f19b67
jcha0713 <joocha0713@gmail.com>
2022-01-18 09:15:04 -0800

    change emmet leader key

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,2 +1,2 @@
-vim.g['user_emmet_leader_key'] = '<C-space>'
-vim.g['user_emmet_settings'] = [[{'astro': {'extends' : 'html',}}]]
+vim.g["user_emmet_leader_key"] = "<C-e>"
+vim.g["user_emmet_settings"] = [[{'astro': {'extends' : 'html',}}]]
