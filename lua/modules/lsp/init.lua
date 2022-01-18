9c50e18155e615ab5371b38f3c2533c9654a9671
jcha0713 <joocha0713@gmail.com>
2022-01-18 09:14:37 -0800

    change border for diagnostic popup

--- lua/modules/lsp/init.lua
+++ lua/modules/lsp/init.lua
@@ -85,6 +85,24 @@ configs[server_name] = {
 
 require("modules.lsp.astro").setup(on_attach)
 
+local border = {
+  { "╭", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╮", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "╯", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╰", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 vim.diagnostic.config {
   virtual_text = false,
   signs = true,
