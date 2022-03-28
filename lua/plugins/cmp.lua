3ab261f91939380c423cfd82a7cb0f61c6ab4464
jcha0713 <joocha0713@gmail.com>
2022-03-27 19:06:05 -0700

    clean up && update keymappings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,43 +18,23 @@ cmp.setup {
         buffer = "[Buf]",
         nvim_lsp = "[LSP]",
         luasnip = "[Snip]",
-        nvim_lua = "[Lua]",
+        nvim_lua = "[Nvim]",
         look = "[Dict]",
       },
     },
   },
   mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-u>"] = cmp.mapping.scroll_docs(4),
-    -- ["<C-space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm(),
-    --[[ ["<C-f>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }, ]]
-    -- ["<tab>"] = cmp.mapping(function(fallback)
-    --   if ls.expand_or_jumpable() then
-    --     ls.expand_or_jump()
-    --   else
-    --     fallback()
-    --   end
-    -- end, { "i", "s" }),
-    -- ["<s-tab>"] = cmp.mapping(function(fallback)
-    --   if ls.jumpable(-1) then
-    --     ls.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, { "i", "s" }),
   },
   sources = {
     { name = "luasnip" },
     { name = "nvim_lua" },
     { name = "nvim_lsp" },
-    -- { name = "cmp_tabnine" },
     { name = "path" },
     {
       name = "fuzzy_buffer",
@@ -64,7 +44,7 @@ cmp.setup {
     { name = "neorg" },
     {
       name = "look",
-      keyword_length = 2,
+      keyword_length = 5,
       max_item_count = 4,
       option = {
         convert_case = true,
