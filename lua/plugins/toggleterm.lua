e6c51c4015b113d2a28c7c43b4765c8e549cfcc4
jcha0713 <joocha0713@gmail.com>
2022-03-27 18:01:22 -0700

    add some new plugins

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+require("toggleterm").setup {
+  -- size can be a number or function which is passed the current terminal
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 10
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
+  open_mapping = [[<C-\>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  shade_filetypes = {},
+  shade_terminals = false,
+  shading_factor = 3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  start_in_insert = true,
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  persist_size = true,
+  direction = "horizontal", -- 'horizontal' or 'vertical'
+  close_on_exit = true, -- close the terminal window when the process exits
+  -- shell = vim.o.shell, -- change the default shell
+}
---
+++ lua/plugins/vim-rooter.lua
@@ -0,0 +1,8 @@
+vim.g.rooter_patterns = {
+  -- "prettierrc.json",
+  -- "tsconfig.json",
+  -- "stylua.toml",
+  -- ".git",
+}
+
+vim.g.rooter_change_directory_for_non_project_files = "current"
