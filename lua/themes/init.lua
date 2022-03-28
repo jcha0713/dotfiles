3cae41e6e08f35097f57b06ca998cdc2e07c2538
jcha0713 <joocha0713@gmail.com>
2022-03-27 18:02:16 -0700

    manage colorscheme in themes/init.lua

---
+++ lua/themes/init.lua
@@ -0,0 +1,28 @@
+-- vim.g.gruvbox_transparent = true
+
+vim.g.gruvbox_colors = {
+  comment = "#888888",
+  bg = "#262626",
+}
+
+-- override a few theme colors
+vim.g.gruvbox_theme = {
+  NormalFloat = { bg = "none" },
+  -- change highlight colors for hop.nvim
+  HopNextKey = { fg = "#ff9900" },
+  HopNextKey1 = { fg = "#ff9900" },
+  HopNextKey2 = { fg = "#ff9900" },
+}
+
+vim.g.gruvbox_flat_style = "dark"
+vim.g.gruvbox_hideInactiveStatusline = true
+
+-- set colorscheme to gruvbox-flat
+vim.cmd [[colorscheme gruvbox-flat]]
+
+-- highlight for comments
+vim.cmd [[highlight Visual guifg=#f7f6f0 guibg=#8a6363]]
+
+-- highlight for cmp: matching characters
+vim.cmd [[highlight! CmpItemAbbrMatch guibg=NONE guifg=#d3869b]]
+vim.cmd [[highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#d3869b]]
