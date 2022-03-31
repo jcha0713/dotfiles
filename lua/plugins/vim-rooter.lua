1436a946ab3df7c24146e624e270a9c847935e81
jcha0713 <joocha0713@gmail.com>
2022-03-30 22:51:33 -0700

    fix the issue with neogit: now git commit works well!

--- lua/plugins/vim-rooter.lua
+++ lua/plugins/vim-rooter.lua
@@ -1,8 +1,6 @@
 vim.g.rooter_patterns = {
-  -- "prettierrc.json",
-  -- "tsconfig.json",
-  -- "stylua.toml",
-  -- ".git",
+  "!.git/worktrees", -- without this line, git commit in neogit does not work well because vim-rooter is changing the cwd
+  ".git",
 }
 
 vim.g.rooter_change_directory_for_non_project_files = "current"
