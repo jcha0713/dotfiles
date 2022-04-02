87175be4231a4e16e658b5908167ea854ce0a02b
jcha0713 <joocha0713@gmail.com>
2022-04-02 11:41:53 -0700

    autopair on cmp completion

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,12 +1,19 @@
-local npairs = require "nvim-autopairs"
-local Rule = require "nvim-autopairs.rule"
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
+end
 
-npairs.setup()
+npairs.setup({
+  check_ts = true,
+})
 
---[[ npairs.add_rules {
-  Rule(
-    "%(.*%)%s*%=>$",
-    " {  }",
-    { "typescript", "typescriptreact", "javascript" }
-  ):use_regex(true):set_end_pair_length(2),
-} ]]
+-- auto insert () for cmp completion
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done({ map_char = { tex = "" } })
+)
