fb50c22af12c31a6dc57fcf6f225f4e525d61ffa
jcha0713 <joocha0713@gmail.com>
2022-04-03 14:10:22 -0700

    Add lazygit integration for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,4 +1,4 @@
-require("toggleterm").setup {
+require("toggleterm").setup({
   -- size can be a number or function which is passed the current terminal
   size = function(term)
     if term.direction == "horizontal" then
@@ -13,9 +13,45 @@ require("toggleterm").setup {
   shade_terminals = false,
   shading_factor = 3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  insert_mappings = false, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
   direction = "horizontal", -- 'horizontal' or 'vertical'
   close_on_exit = true, -- close the terminal window when the process exits
-}
+})
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "curved",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.keymap.set(
+      -- term.bufnr,
+      "n",
+      "q",
+      "<cmd>close<CR>",
+      { noremap = true, silent = true, buffer = term.bufnr }
+    )
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("Closing terminal")
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.keymap.set(
+  "n",
+  "<leader>lg",
+  "<cmd>lua _lazygit_toggle()<CR>",
+  { noremap = true, silent = true }
+)
