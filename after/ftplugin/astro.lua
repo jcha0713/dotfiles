fd80fe32babb6b050121c57ddf73c830eddacc87
jcha0713 <joocha0713@gmail.com>
2022-04-06 14:05:02 -0700

    parentheses

--- after/ftplugin/astro.lua
+++ after/ftplugin/astro.lua
@@ -1,5 +1,5 @@
-vim.cmd "syntax on"
-vim.cmd "set syntax=typescriptreact"
+vim.cmd("syntax on")
+vim.cmd("set syntax=typescriptreact")
 local api = vim.api
 
 -- temporary fix for astro files syntax highlighting
@@ -7,7 +7,7 @@ local astro = api.nvim_create_augroup("astro", { clear = true })
 api.nvim_create_autocmd("FileType", {
   pattern = "astro",
   callback = function()
-    api.nvim_command "edit"
+    api.nvim_command("edit")
   end,
   group = astro,
   desc = "Fix syntax highlighting for astro files",
