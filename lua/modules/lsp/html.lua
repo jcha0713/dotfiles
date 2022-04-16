b100704edd852cfe7a9360fb9e0a5f4a9100382b
jcha0713 <joocha0713@gmail.com>
2022-04-15 22:24:51 -0700

    set html document formatting to false

--- lua/modules/lsp/html.lua
+++ lua/modules/lsp/html.lua
@@ -6,6 +6,8 @@ M.setup = function(on_attach, capabilities)
   lspconfig.html.setup({
     on_attach = function(client)
       on_attach(client)
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
     end,
     capabilities = capabilities or {},
     cmd = { "vscode-html-language-server", "--stdio" },
