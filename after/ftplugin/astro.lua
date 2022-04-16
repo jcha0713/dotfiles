78eacc50eccc946f84de73f4078cdd4ecc79d5ce
jcha0713 <joocha0713@gmail.com>
2022-04-15 22:22:18 -0700

    Astro parser was added to treesitter so I don't need this anymore

--- after/ftplugin/astro.lua
+++ after/ftplugin/astro.lua
@@ -3,12 +3,12 @@ vim.cmd("set syntax=typescriptreact")
 local api = vim.api
 
 -- temporary fix for astro files syntax highlighting
-local astro = api.nvim_create_augroup("astro", { clear = true })
-api.nvim_create_autocmd("FileType", {
-  pattern = "astro",
-  callback = function()
-    api.nvim_command("edit")
-  end,
-  group = astro,
-  desc = "Fix syntax highlighting for astro files",
-})
+-- local astro = api.nvim_create_augroup("astro", { clear = true })
+-- api.nvim_create_autocmd("FileType", {
+--   pattern = "astro",
+--   callback = function()
+--     api.nvim_command("edit")
+--   end,
+--   group = astro,
+--   desc = "Fix syntax highlighting for astro files",
+-- })
