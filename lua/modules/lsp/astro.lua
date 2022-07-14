4f61282760a3a8cf9cf0aab904b3abaa617b61d5
jcha0713 <joocha0713@gmail.com>
2022-07-13 18:25:18 -0700

    Delete Astro LSP settings as they are not needed in the new version of language-server

--- lua/modules/lsp/astro.lua
+++ lua/modules/lsp/astro.lua
@@ -6,60 +6,6 @@ M.setup = function(on_attach, capabilities)
   lspconfig.astro.setup({
     on_attach = on_attach,
     capabilities = capabilities or {},
-
-    -- this configuration needs to be provided to work with @astrojs/language-server@0.16.1 or greater.
-    settings = {
-      astro = {
-        astro = {
-          enabled = true,
-          diagnostics = { enabled = true },
-          rename = { enabled = true },
-          format = { enabled = true },
-          completions = { enabled = true },
-          hover = { enabled = true },
-          codeActions = { enabled = true },
-          selectionRange = { enabled = true },
-        },
-
-        typescript = {
-          enabled = true,
-          diagnostics = { enabled = true },
-          hover = { enabled = true },
-          completions = { enabled = true },
-          definitions = { enabled = true },
-          findReferences = { enabled = true },
-          documentSymbols = { enabled = true },
-          codeActions = { enabled = true },
-          rename = { enabled = true },
-          selectionRange = { enabled = true },
-          signatureHelp = { enabled = true },
-          semanticTokens = { enabled = true },
-          implementation = { enabled = true },
-          typeDefinition = { enabled = true },
-        },
-
-        css = {
-          enabled = true,
-          diagnostics = { enabled = true },
-          hover = { enabled = true },
-          completions = { enabled = true, emmet = true },
-          documentColors = { enabled = true },
-          colorPresentations = { enabled = true },
-          documentSymbols = { enabled = true },
-          selectionRange = { enabled = true },
-        },
-
-        html = {
-          enabled = true,
-          hover = { enabled = true },
-          completions = { enabled = true, emmet = true },
-          tagComplete = { enabled = true },
-          documentSymbols = { enabled = true },
-          renameTags = { enabled = true },
-          linkedEditing = { enabled = true },
-        },
-      },
-    },
   })
 end
 
