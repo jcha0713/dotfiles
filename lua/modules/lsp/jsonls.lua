5ac692b6898430844fed4231602157c612faac01
jcha0713 <joocha0713@gmail.com>
2022-07-14 20:09:11 -0700

    Add json schemas

--- lua/modules/lsp/jsonls.lua
+++ lua/modules/lsp/jsonls.lua
@@ -4,7 +4,13 @@ M.setup = function(on_attach, capabilities)
   require("lspconfig").jsonls.setup({
     settings = {
       json = {
-        schemas = require("schemastore").json.schemas(),
+        schemas = require("schemastore").json.schemas({
+          select = {
+            "tsconfig.json",
+            "package.json",
+            "prettierrc.json",
+          },
+        }),
         validate = { enable = true },
       },
     },
