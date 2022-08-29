0a9318b447a64919ca84d8fefae0425bb7a1c968
jcha0713 <joocha0713@gmail.com>
2022-08-28 21:40:18 -0700

    Clean up

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -2,28 +2,3 @@ local api = vim.api
 
 api.nvim_buf_set_keymap(0, "n", "j", "gj", { noremap = true, silent = true })
 api.nvim_buf_set_keymap(0, "n", "k", "gk", { noremap = true, silent = true })
-
--- vim.cmd([[
--- let g:markdown_fenced_languages = ['javascript', 'js=javascript']
--- ]])
-
--- vim.opt.conceallevel = 2
-vim.cmd([[
-let g:pencil#wrapModeDefault = 'soft'
-
-augroup pencil
-  autocmd!
-  autocmd FileType markdown,mkd call pencil#init()
-  autocmd FileType text         call pencil#init()
-augroup END
-]])
-
-local writegood = api.nvim_create_augroup("writegood", { clear = true })
-api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    api.nvim_command("WritegoodEnable")
-  end,
-  group = writegood,
-  desc = "Enables writegood plugin for markdown files",
-})
