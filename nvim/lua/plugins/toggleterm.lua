003444b17db2372f3d7cf0800a6b768dd15d7d91
jcha0713 <joocha0713@gmail.com>
2022-09-07 23:50:01 -0700

    Chnage toggleterm height

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ require("toggleterm").setup({
   -- size can be a number or function which is passed the current terminal
   size = function(term)
     if term.direction == "horizontal" then
-      return 10
+      return 20
     elseif term.direction == "vertical" then
       return vim.o.columns * 0.4
     end
