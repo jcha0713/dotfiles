8814054c7f2c7ff0fa7445b7a79b9e598500c5a9
jcha0713 <joocha0713@gmail.com>
2022-10-16 19:21:55 -0700

    Use impatient for faster initial load

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,14 @@
--- General settings
 vim.g.ts_highlight_lua = true
 
+local imp_ok, _ = pcall(require, "impatient")
+if not imp_ok then
+  return
+end
+
+--General settings
 require("plugins")
-require("modules.options")
 require("modules.keymappings")
+require("modules.options")
 require("modules.utils")
 require("modules.autocmd")
 require("modules.globals")
