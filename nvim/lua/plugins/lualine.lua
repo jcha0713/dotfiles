df3b77c606b8302c0a3c643f2748d46e5f265e7b
jcha0713 <joocha0713@gmail.com>
2022-11-29 23:48:04 -0800

    Display linter info

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,32 @@
 local lualine = require("lualine")
 
+local function lsp_client()
+  local buf_ft = vim.bo.filetype
+  local buf_clients = vim.lsp.get_active_clients()
+  local buf_client_names = {}
+
+  -- add client
+  for _, client in pairs(buf_clients) do
+    if client.name == "eslint" then
+      table.insert(buf_client_names, client.name)
+    end
+  end
+
+  -- add linter
+  local supported_linters = require("modules.lsp.null-ls").list_registered(
+    buf_ft
+  )
+  vim.list_extend(buf_client_names, supported_linters)
+
+  return "[" .. table.concat(buf_client_names, ", ") .. "]"
+end
+
 lualine.setup({
   options = {
     icons_enabled = true,
     theme = "kanagawa",
-    section_separators = "",
+    -- section_separators = "",
+    section_separators = { left = "", right = "" },
     component_separators = "",
     always_divide_middle = true,
     globalstatus = true,
@@ -15,6 +37,7 @@ lualine.setup({
       "branch",
       "diff",
       { "diagnostics", sources = { "nvim_diagnostic" } },
+      { lsp_client },
     },
     lualine_c = { "filename" },
     lualine_x = { "encoding", "fileformat", "filetype" },
