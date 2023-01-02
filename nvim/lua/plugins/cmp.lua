1163e5339d6149828acd2117bd506bb60df32a6d
jcha0713 <joocha0713@gmail.com>
2023-01-01 22:09:34 -0800

    Better cmp ui

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,6 +1,11 @@
 local cmp = require("cmp")
-local compare = require("cmp.config.compare")
 local lspkind = require("lspkind")
+local compare = require("cmp.config.compare")
+local kanagawa = require("kanagawa.colors").setup()
+
+vim.api.nvim_set_hl(0, "CmpCursorLine", { bg = "#a390a2", fg = "#171717" })
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = kanagawa.oniViolet })
+vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = kanagawa.surimiOrange })
 
 cmp.setup({
   snippet = {
@@ -8,16 +13,25 @@ cmp.setup({
       require("luasnip").lsp_expand(args.body)
     end,
   },
+  window = {
+    completion = cmp.config.window.bordered({
+      winhighlight = "CursorLine:CmpCursorLine",
+    }),
+    documentation = cmp.config.window.bordered(),
+  },
+  view = {
+    entries = { name = "custom", selection_order = "near_cursor" },
+  },
   formatting = {
     format = lspkind.cmp_format({
       with_text = true,
       maxwidth = math.floor(vim.api.nvim_win_get_width(0) / 2),
       maxheight = math.floor(vim.api.nvim_win_get_height(0) / 3 * 2),
       menu = {
-        ["cmp-tw2css"] = "[TailwindCSS]",
         nvim_lsp = "[LSP]",
-        luasnip = "[Snip]",
         fuzzy_buffer = "[Buf]",
+        ["cmp-tw2css"] = "[TailwindCSS]",
+        luasnip = "[Snip]",
         look = "[Dict]",
       },
     }),
@@ -34,32 +48,33 @@ cmp.setup({
   --   ghost_text = { hl_group = "CmpItemAbbrMatch" },
   -- },
   sorting = {
-    priority_weight = 1,
+    priority_weight = 100,
     comparators = {
-      require("cmp_fuzzy_buffer.compare"),
-      -- compare.offset,
-      -- compare.exact,
-      -- compare.score,
-      -- compare.recently_used,
+      compare.locality,
+      compare.recently_used,
+      compare.score,
+      compare.exact,
       compare.kind,
+      -- require("cmp_fuzzy_buffer.compare"),
+      -- compare.offset,
       -- compare.sort_text,
       -- compare.length,
       -- compare.order,
     },
   },
   sources = cmp.config.sources({
-    { name = "cmp-tw2css" },
     { name = "nvim_lsp" },
-    {
-      name = "luasnip",
-    },
+    { name = "cmp-tw2css" },
     {
       name = "fuzzy_buffer",
-      keyword_length = 5,
+      keyword_length = 3,
       max_item_count = 10,
     },
     { name = "path" },
     { name = "neorg" },
+    {
+      name = "luasnip",
+    },
     {
       name = "look",
       keyword_length = 5,
