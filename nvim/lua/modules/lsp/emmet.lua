fd72b68a462b486c7c7d5e2c432e8b5c2bdbb32f
jcha0713 <joocha0713@gmail.com>
2023-03-22 09:19:45 +0900

    lint

--- nvim/lua/modules/lsp/emmet.lua
+++ nvim/lua/modules/lsp/emmet.lua
@@ -1,18 +1,23 @@
 local lspconfig = require("lspconfig")
-local util = require 'lspconfig.util'
+local util = require("lspconfig.util")
 
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-    lspconfig.emmet_language_server.setup({
-      on_attach = function(client)
-        on_attach(client)
-      end,
-      capabilities = capabilities,
-      root_dir = function(fname)
-        return util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git')(fname)
-      end,
-    })
+  lspconfig.emmet_language_server.setup({
+    on_attach = function(client)
+      on_attach(client)
+    end,
+    capabilities = capabilities,
+    root_dir = function(fname)
+      return util.root_pattern(
+        "package.json",
+        "tsconfig.json",
+        "jsconfig.json",
+        ".git"
+      )(fname)
+    end,
+  })
 end
 
 return M
