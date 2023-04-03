6ade38baf0318725e6d3d6c06a3b272427291f88
jcha0713 <joocha0713@gmail.com>
2023-04-03 17:40:34 +0900

    lint

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 return {
   "kyazdani42/nvim-tree.lua",
   keys = {
-    { "<C-n>", ":NvimTreeToggle<CR>", desc = "Open NvimTree" }
+    { "<C-n>", ":NvimTreeToggle<CR>", desc = "Open NvimTree" },
   },
   config = function()
     require("nvim-tree").setup({
