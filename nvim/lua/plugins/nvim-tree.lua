ee5ad66d201501b615ca07c4e84294ca4acfebd9
jcha0713 <joocha0713@gmail.com>
2023-04-14 14:07:14 +0900

    Remove deprecated ones

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -7,9 +7,7 @@ return {
     require("nvim-tree").setup({
       -- disable_netrw = true,
       -- hijack_netrw = true,
-      open_on_setup = false,
       respect_buf_cwd = false,
-      ignore_ft_on_setup = {},
       open_on_tab = false,
       hijack_cursor = false,
       update_cwd = true,
