c8b421f5c7cc46fbde5e2078157c68731fd67b93
jcha0713 <joocha0713@gmail.com>
2023-04-27 21:21:20 +0900

    load on bufread

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = "nvim-lua/plenary.nvim",
+  event = "BufRead",
   config = function()
     require("todo-comments").setup({
       search = {
