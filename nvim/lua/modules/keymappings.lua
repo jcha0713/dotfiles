6dc44b5fb65cc96dc17ba950378e45f23d1f87c5
jcha0713 <joocha0713@gmail.com>
2023-04-27 21:02:38 +0900

    Add some descriptions

--- nvim/lua/modules/keymappings.lua
+++ nvim/lua/modules/keymappings.lua
@@ -10,8 +10,8 @@ map("n", "<leader>q", "<cmd>qa<cr>")
 map("i", "jj", "<C-[>")
 
 -- split windows
-map("n", "|", "<cmd>vnew<cr>")
-map("n", "_", "<cmd>new<cr>")
+map("n", "|", "<cmd>vnew<cr>", { desc = "Split window vertically" })
+map("n", "_", "<cmd>new<cr>", { desc = "Split window horizontally" })
 
 -- manage window size
 map("n", "<C-w>+", "5<C-w>+")
@@ -36,13 +36,13 @@ map("v", "<C-k>", ":m '<-2<CR>gv=gv")
 map("n", "vv", "<S-v>")
 
 -- Grammar checking in the terminal(using gramma)
-map("n", "<leader>gc", ":term gramma check %<cr>")
+map("n", "<leader>gc", ":term gramma check %<cr>", { desc = "Check grammar" })
 
 -- LspRestart
-map("n", "<leader>rs", ":LspRestart<CR>:e<CR>")
+map("n", "<leader>rs", ":LspRestart<CR>:e<CR>", { desc = "Restart LSP" })
 
 -- global yank
-map("n", "gy", "ggyG")
+map("n", "gy", "ggyG", { desc = "Yank all lines in file" })
 
 -- quickfix
 -- map("n", "Q", ":cw<CR>")
@@ -51,14 +51,14 @@ map("n", "[c", ":cp<CR>")
 
 -- F and L for first and last character movement
 -- and now tesing gh and gl
-map("n", "gh", "^")
-map("n", "gl", "$")
-map("n", "dgh", "d^")
-map("n", "dgl", "d$")
-map("n", "cgh", "c^")
-map("n", "cgl", "c$")
-map("n", "vgh", "v^")
-map("n", "vgl", "v$")
+map("n", "gh", "^", { desc = "Move to first character in line" })
+map("n", "gl", "$", { desc = "Move to last character in line" })
+map("n", "dgh", "d^", { desc = "Delete to first character in line" })
+map("n", "dgl", "d$", { desc = "Delete to last character in line" })
+map("n", "cgh", "c^", { desc = "Change to first character in line" })
+map("n", "cgl", "c$", { desc = "Change to last character in line" })
+map("n", "vgh", "v^", { desc = "Select to first character in line" })
+map("n", "vgl", "v$", { desc = "Select to last character in line" })
 
 -- better window movement
 map("n", "<BS>", "<C-w>h")
@@ -67,16 +67,16 @@ map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
 -- Switch buffers
-map("n", "]b", ":bnext<CR>")
-map("n", "[b", ":bprevious<CR>")
-map("n", "<Leader>x", ":bd<CR>")
+map("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
+map("n", "[b", ":bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<Leader>x", ":bd<CR>", { desc = "Close buffer" })
 
 -- Tab navigation
-map("n", "<leader>to", ":tabnew<CR>")
-map("n", "<leader>ts", ":tab split<CR>")
-map("n", "<leader>tc", ":tabclose<CR>")
-map("n", "]t", ":tabnext<CR>")
-map("n", "[t", ":tabprevious<CR>")
+map("n", "<leader>to", ":tabnew<CR>", { desc = "Open new tab" })
+map("n", "<leader>ts", ":tab split<CR>", { desc = "Split tab" })
+map("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab" })
+map("n", "]t", ":tabnext<CR>", { desc = "Next tab" })
+map("n", "[t", ":tabprevious<CR>", { desc = "Previous tab" })
 
 -- set break points for undos
 map("i", ",", ",<C-g>u")
@@ -97,3 +97,5 @@ map("i", "<A-,>", "<esc>la,")
 
 -- add colon and jump
 map("i", "<A-;>", "<esc>la:")
+
+map("n", "<C-p>", 'viwp"_dP', { desc = "Paste over selected text" })
