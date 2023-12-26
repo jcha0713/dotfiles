a5a0ae58606579e384601a3b58422ed2bf1d7b18
jcha0713 <joocha0713@gmail.com>
2023-12-26 21:00:44 +0900

    load snippets in markdown files

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,4 +3,8 @@ local api = vim.api
 api.nvim_buf_set_keymap(0, "n", "j", "gj", { noremap = true, silent = true })
 api.nvim_buf_set_keymap(0, "n", "k", "gk", { noremap = true, silent = true })
 
+require("luasnip/loaders/from_vscode").lazy_load({
+  paths = { "~/.config/nvim/friendly-snippets/" },
+})
+
 -- vim.opt_global.formatoptions = "aw"
