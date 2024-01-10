e60190ae40a5f2b7419aeb2bef225ee3e405dee2
jcha0713 <joocha0713@gmail.com>
2024-01-10 19:51:01 +0900

    update neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,5 +1,10 @@
 return {
-  "TimUntersberger/neogit",
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
   cmd = "Neogit",
   keys = {
     {
