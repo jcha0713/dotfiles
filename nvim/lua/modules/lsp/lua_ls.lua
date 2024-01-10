0660a40be66c76bd93573da7d2437d8148021c73
jcha0713 <joocha0713@gmail.com>
2024-01-11 01:11:39 +0900

    improve lua_ls initial indexing speed

--- nvim/lua/modules/lsp/lua_ls.lua
+++ nvim/lua/modules/lsp/lua_ls.lua
@@ -6,35 +6,51 @@ M.setup = function(on_attach, capabilities)
   lspconfig.lua_ls.setup({
     on_attach = on_attach,
     capabilities = capabilities or {},
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = {
-            "vim",
-            "use",
-            "describe",
-            "it",
-            "assert",
-            "before_each",
-            "after_each",
-            "hs", -- hammerspoon
-          },
-        },
-        completion = {
-          showWord = "Disable",
-          callSnippet = "Disable",
-          keywordSnippet = "Disable",
-        },
-        workspace = {
-          checkThirdParty = false,
-          preloadFileSize = 100000,
-          maxPreload = 10000,
-        },
-      },
-    },
+    on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if
+        not vim.loop.fs_stat(path .. "/.luarc.json")
+        and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+      then
+        client.config.settings =
+          vim.tbl_deep_extend("force", client.config.settings, {
+            Lua = {
+              runtime = {
+                version = "LuaJIT",
+              },
+              diagnostics = {
+                globals = {
+                  "vim",
+                  "use",
+                  "describe",
+                  "it",
+                  "assert",
+                  "before_each",
+                  "after_each",
+                  "hs", -- hammerspoon
+                },
+              },
+              workspace = {
+                checkThirdParty = false,
+                preloadFileSize = 100000,
+                maxPreload = 10000,
+                library = {
+                  vim.env.VIMRUNTIME,
+                },
+              },
               hint = {
                 enable = true,
               },
+            },
+          })
+
+        client.notify(
+          "workspace/didChangeConfiguration",
+          { settings = client.config.settings }
+        )
+      end
+      return true
+    end,
   })
 end
 
