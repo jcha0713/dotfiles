109f6e2c083ae6dd0c41976d22ece49e9352e868
jcha0713 <joocha0713@gmail.com>
2024-01-10 17:53:05 +0900

    add preview image feature to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -108,6 +108,40 @@ return {
       pickers = picker_opt,
       defaults = {
         file_ignore_patterns = ignore_files,
+        preview = { -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#use-terminal-image-viewer-to-preview-images
+          treesitter = true,
+          mime_hook = function(filepath, bufnr, opts)
+            local is_image = function(filepath)
+              local image_extensions = { "png", "jpg" } -- Supported image formats
+              local split_path =
+                vim.split(filepath:lower(), ".", { plain = true })
+              local extension = split_path[#split_path]
+              return vim.tbl_contains(image_extensions, extension)
+            end
+            if is_image(filepath) then
+              local term = vim.api.nvim_open_term(bufnr, {})
+              local function send_output(_, data, _)
+                for _, d in ipairs(data) do
+                  vim.api.nvim_chan_send(term, d .. "\r\n")
+                end
+              end
+              vim.fn.jobstart({
+                "catimg",
+                filepath, -- Terminal image viewer command
+              }, {
+                on_stdout = send_output,
+                stdout_buffered = true,
+                pty = true,
+              })
+            else
+              require("telescope.previewers.utils").set_preview_message(
+                bufnr,
+                opts.winid,
+                "Binary cannot be previewed"
+              )
+            end
+          end,
+        },
         layout_config = {
           width = 0.90,
           prompt_position = "top",
@@ -133,20 +167,6 @@ return {
         layout_strategy = "horizontal",
         file_sorter = require("telescope.sorters").get_fuzzy_file,
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-        path_display = {},
-        winblend = 0,
-        border = {},
-        borderchars = {
-          "─",
-          "│",
-          "─",
-          "│",
-          "╭",
-          "╮",
-          "╯",
-          "╰",
-        },
-        color_devicons = true,
         set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
         file_previewer = require("telescope.previewers").vim_buffer_cat.new,
         grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
