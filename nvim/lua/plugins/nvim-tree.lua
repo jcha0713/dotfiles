591ff264a7d0266c2f30fa11bdc86902d3bc232a
jcha0713 <joocha0713@gmail.com>
2024-01-26 21:49:41 +0900

    focus on current buffer file when opening nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -8,7 +8,18 @@ return {
       sync_root_with_cwd = true,
       respect_buf_cwd = true,
       view = {
+        centralize_selection = true,
         side = "right",
+      },
+      renderer = {
+        highlight_bookmarks = "name",
+      },
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+      modified = {
+        enable = true,
       },
     })
   end,
