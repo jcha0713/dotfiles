59da34f192ce66bc94305b4fc9f3775c57977da6
jcha0713 <joocha0713@gmail.com>
2024-02-25 04:07:45 +0900

    Refactor auto_commit to handle untracked files

--- nvim/lua/plugins/custom/auto_commit.lua
+++ nvim/lua/plugins/custom/auto_commit.lua
@@ -1,39 +1,71 @@
 local M = {}
 
-local function handle_exit(obj)
-  if obj.code == 1 then
-    vim.print(obj.stderr)
+local filename = vim.fn.expand("%:p")
+
+local function handle_exit(sgpt_result)
+  if sgpt_result.code == 1 then
+    vim.print(sgpt_result.stderr)
   end
-  if obj.code == 0 then
+  if sgpt_result.code == 0 then
     vim.schedule(function()
       -- TODO: implement EDIT
-      local confirm =
-        vim.fn.confirm("Commit with generated message?", "&Yes\n&Edit\n&Cancel")
+      local confirm = vim.fn.confirm(
+        "Generated Message:\n================\n"
+          .. sgpt_result.stdout
+          .. "\n================\nCommit with this message?",
+        "&Yes\n&Edit\n&Cancel"
+      )
       if confirm == 1 then
-        vim.print(obj.stdout)
+        vim
+          .system({
+            "sh",
+            "-c",
+            string.format(
+              "git add '%s' && git commit -m '%s'",
+              filename,
+              sgpt_result.stdout
+            ),
+          })
+          :wait()
       end
     end)
   end
 end
 
 function M.generate_commit_message()
-  local filename = vim.fn.expand("%:p")
-
   if type(filename) ~= "string" then
     return
   end
 
-  -- TODO: handle untracked files
-  local cmd = {
-    "sh",
-    "-c",
-    string.format(
-      "git diff -- '%s' | sgpt 'Summarize git diff changes for a commit message in under 80 chars'",
-      filename
-    ),
-  }
+  local untracked_files_cmd =
+    { "git", "ls-files", "--others", "--exclude-standard" }
+  local untracked_files_output =
+    vim.system(untracked_files_cmd, { text = true }):wait()
 
-  vim.system(cmd, { text = true }, handle_exit)
+  if untracked_files_output.code == 0 then
+    local is_untracked = string.find(
+      untracked_files_output.stdout,
+      vim.fn.fnamemodify(filename, ":t")
+    )
+    local diff_cmd
+    if is_untracked then
+      diff_cmd = string.format(
+        "git diff --no-index -- /dev/null '%s' | sgpt 'Write a concise git commit message in under 80 characters for me'",
+        filename
+      )
+    else
+      diff_cmd = string.format(
+        "git diff HEAD -- '%s' | sgpt 'Write a concise git commit message in under 80 characters for me'",
+        filename
+      )
+    end
+
+    vim.system({
+      "sh",
+      "-c",
+      diff_cmd,
+    }, { text = true }, handle_exit)
+  end
 end
 
 return M
