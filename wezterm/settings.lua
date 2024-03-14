766933d4f4b6093865a4e1ddb77fc9e0aee76222
jcha0713 <joocha0713@gmail.com>
2024-03-14 15:05:24 +0900

    use kitty keyboard protocol

--- wezterm/settings.lua
+++ wezterm/settings.lua
@@ -1,5 +1,8 @@
 local M = {}
 
-M.set_settings = function(config) end
+M.set_settings = function(config)
+  config.enable_kitty_keyboard = true
+  config.enable_csi_u_key_encoding = false
+end
 
 return M
