6b42534c3f41531fb5f2f8d1a7df4a63478fbe41
jcha0713 <joocha0713@gmail.com>
2024-05-24 14:39:58 +0900

    remove deprecated `sign_define()`

--- nvim/lua/modules/lsp/init.lua
+++ nvim/lua/modules/lsp/init.lua
@@ -34,7 +34,20 @@ vim.diagnostic.config({
     show_header = true,
   },
   virtual_text = false,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
+    },
+  },
   underline = true,
   update_in_insert = false,
   severity_sort = false,
