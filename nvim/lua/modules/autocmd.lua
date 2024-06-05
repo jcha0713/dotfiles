9ed8e2cfce030406f914510cc0fb87a2768b18be
jcha0713 <joocha0713@gmail.com>
2024-06-05 22:48:36 +0900

    return to last position when opening files

--- nvim/lua/modules/autocmd.lua
+++ nvim/lua/modules/autocmd.lua
@@ -87,3 +87,15 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 
+local last_position = augroup("misc", { clear = true })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Open file at the last position it was edited earlier",
+  group = last_position,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
