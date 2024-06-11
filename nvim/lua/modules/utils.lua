184c023f9e54ec1a4dbe0f177067e8be47cff7c8
jcha0713 <joocha0713@gmail.com>
2024-06-11 23:41:28 +0900

    new utils

--- nvim/lua/modules/utils.lua
+++ nvim/lua/modules/utils.lua
@@ -3,6 +3,53 @@ local api = vim.api
 
 local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
 
+--- Returns the first argument which is not nil.
+---
+--- If all arguments are nil, returns nil.
+---
+--- Examples:
+---
+--- ```lua
+--- local a = nil
+--- local b = nil
+--- local c = 42
+--- local d = true
+--- assert(vim.F.if_nil(a, b, c, d) == 42)
+--- ```
+---
+--- Copied from [neovim core](https://github.com/neovim/neovim/blob/master/runtime/lua/vim/F.lua)
+--- This will likely be deprecated in the future.
+---
+---@generic T
+---@param ... T
+---@return T
+function utils.if_nil(...)
+  local nargs = select("#", ...)
+  for i = 1, nargs do
+    local v = select(i, ...)
+    if v ~= nil then
+      return v
+    end
+  end
+  return nil
+end
+
+--- display macOS keys nicely
+---@param keymap string
+---@return string ...
+function utils.to_macos_keys(keymap)
+  return keymap
+    :gsub("CR", "↩")
+    :gsub("<", "")
+    :gsub(">", "")
+    :gsub("-", " ")
+    :gsub("D", "⌘")
+    :gsub("A", "⌥")
+    :gsub("C", "⌃")
+    :gsub("BS", "⌫")
+    :gsub("leader", vim.g.mapleader .. " ")
+end
+
 local function get_map_options(custom_options)
   local options = { noremap = true, silent = true }
   if custom_options then
