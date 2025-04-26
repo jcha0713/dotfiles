101a268790e688afffed6e14f054dfb167c29fdc
jcha0713 <joocha0713@gmail.com>
2025-04-26 20:27:34 +0900

    nv: use built-in commenting
    add ts-comments and disable comment.nvim

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,6 @@
 return {
   "numToStr/Comment.nvim",
+  enabled = false,
   lazy = false,
   config = function()
     require("Comment").setup()
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -605,4 +605,11 @@ return {
       require("better-type-hover").setup()
     end,
   },
+
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  },
 }
