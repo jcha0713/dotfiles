f29868513afa51f76f4cd83c22449c84cf7e8d71
jcha0713 <joocha0713@gmail.com>
2025-05-06 15:41:56 +0900

    hm: add nb

--- home.nix
+++ home.nix
@@ -52,6 +52,15 @@ in
       catimg wakeonlan tree
       nap zk yazi
       fx zsh-autosuggestions
+      (nb.overrideAttrs (oldAttrs: {
+        version = "7.17.0";
+        src = fetchFromGitHub {
+          owner = "xwmx";
+          repo = "nb";
+          rev = "7.17.0";
+          sha256 = "sha256-gUI7hAZabYPHkSwGtFZxEoi5Hw76fOLYbMZQIvsnSas=";
+        };
+      }))
 
       # Development tools
       git gh git-absorb
