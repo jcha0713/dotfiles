b29d98d43f362b1cd1288c986322a477af9f12a5
jcha0713 <joocha0713@gmail.com>
2025-05-23 11:32:35 +0900

    hm: fix neogit does not show file contents for newly added directories

--- home.nix
+++ home.nix
@@ -131,6 +131,10 @@ in
     enable = true;
     userName = "jcha0713";
     userEmail = "joocha0713@gmail.com";
+    extraConfig = {
+      status.showUntrackedFiles = "all";
+      init.defaultBranch = "main";
+    };
   };
 
   programs.zsh = {
