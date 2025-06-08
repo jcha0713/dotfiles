51a69c6d7405e543ddb94ad48204729138fe3fce
jcha0713 <joocha0713@gmail.com>
2025-06-08 21:05:33 +0900

    hm: add PNPM_HOME env variable

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@ in
       LANG = "en_US.UTF-8";
       ZVM_INIT_MODE = "sourcing"; # SEE: https://github.com/jeffreytse/zsh-vi-mode/issues/277
       ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=#737246";
+      PNPM_HOME = "~/.pnpm";
     };
 
     sessionPath = [
