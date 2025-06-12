c6a57ad8fa548005afe5bc931a800fe2e14114b1
jcha0713 <joocha0713@gmail.com>
2025-06-12 16:24:38 +0900

    nv: fix lsp hover not taking its full width
    nv: fix

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -5,15 +5,5 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
-  }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    code = {
-      width = "block",
-      left_pad = 2,
-      right_pad = 4,
-      border = "thin",
-    },
   },
 }
