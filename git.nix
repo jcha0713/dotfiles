9d7de3b43d5b6547773ccae4b2393cb8b8ff01ac
jcha0713 <joocha0713@gmail.com>
2025-06-27 22:26:24 +0900

    hm: configure git via nix
    - manage global .gitignore in `home.nix`
    - many settings are based on "https://youtu.be/0FkBpQxOPt4?si=tO-RMNXB5fiNW6ZD"

--- git.nix
+++ git.nix
@@ -3,9 +3,47 @@
     enable = true;
     userName = "jcha0713";
     userEmail = "joocha0713@gmail.com";
+    aliases = {
+      undo = "reset HEAD~1 --mixed";
+      st = "status -s";
+      l = "log --oneline --graph --all";
+    };
+    ignores = [
+      ".envrc"
+      ".direnv"
+    ];
     extraConfig = {
       status.showUntrackedFiles = "all";
       init.defaultBranch = "main";
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+      };
+      push = {
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      pull.rebase = true;
+    };
+    delta = {
+      enable = true;
+      options = {
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-decoration-style = "none";
+          file-style = "bold yellow ul";
+        };
+        features = "decorations";
+        whitespace-error-style = "22 reverse";
+      };
+
     };
   };
 }
