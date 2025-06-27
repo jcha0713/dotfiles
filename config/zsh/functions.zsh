cc460df5e336f3939a95090e65d4d5a664f167a1
jcha0713 <joocha0713@gmail.com>
2025-06-27 10:44:06 +0900

    direnv: do git reset after generating `.envrc`

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -4,6 +4,10 @@ function dvt() {
     return 1
   fi
 
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+    git reset .envrc .direnv 2>/dev/null || true
+  fi
+
   if command -v bat &> /dev/null; then
     bat flake.nix
   else
