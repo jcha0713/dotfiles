2aa010e92b9db1344ee37def3330be54a2dcca54
jcha0713 <joocha0713@gmail.com>
2025-06-27 10:44:51 +0900

    hm: extract programs.git into new file

---
+++ git.nix
@@ -0,0 +1,11 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "jcha0713";
+    userEmail = "joocha0713@gmail.com";
+    extraConfig = {
+      status.showUntrackedFiles = "all";
+      init.defaultBranch = "main";
+    };
+  };
+}
--- home.nix
+++ home.nix
@@ -114,15 +114,9 @@ in
     };
   };
 
-  programs.git = {
-    enable = true;
-    userName = "jcha0713";
-    userEmail = "joocha0713@gmail.com";
-    extraConfig = {
-      status.showUntrackedFiles = "all";
-      init.defaultBranch = "main";
-    };
-  };
+  imports = [
+    ./git.nix
+  ];
 
   programs.zsh = {
     enable = true;
