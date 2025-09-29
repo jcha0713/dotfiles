bf9aa2adc51a493aced38f436200fd0a917f3c61
jcha0713 <joocha0713@gmail.com>
2025-09-29 23:07:26 +0900

    nv: plugin clean ups

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -247,8 +247,8 @@ return {
           sorting_strategy = "ascending",
           layout_strategy = "vertical",
           layout_config = {
-            width = 0.8,
-            height = 0.9,
+            width = 0.9,
+            height = 0.5,
             prompt_position = "top",
             preview_cutoff = 20,
             preview_height = function(_, _, max_lines)
@@ -294,7 +294,7 @@ return {
     ft = { "typescript", "typescriptreact" },
     config = function()
       require("better-type-hover").setup({
-        openTypeDocKeymap = "K",
+        openTypeDocKeymap = "H",
       })
     end,
   },
--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,8 +7,6 @@ return {
   },
   dependencies = {
     "b0o/schemastore.nvim",
-    -- hide until https://github.com/ray-x/lsp_signature.nvim/issues/276 is fixed
-    -- "ray-x/lsp_signature.nvim",
   },
   config = function()
     require("modules.lsp")
