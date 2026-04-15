d2b29abd3d2d0b4d566a680a2ebc0efaf038ae0e
jcha0713 <joocha0713@gmail.com>
2026-04-15 22:56:04 +0900

    nvim: add keymaps for mini.visits

--- config/neuvim/config/mini.visits.lua
+++ config/neuvim/config/mini.visits.lua
@@ -1,1 +1,24 @@
 require("mini.visits").setup()
+
+local visits = require("mini.visits")
+local sort_recent = visits.gen_sort.default({ recency_weight = 1 })
+
+vim.keymap.set("n", "ss", function()
+  visits.select_path(nil, { sort = sort_recent })
+end, { desc = "Select recent visited path" })
+
+vim.keymap.set("n", "]s", function()
+  visits.iterate_paths(
+    "forward",
+    vim.fn.getcwd(),
+    { sort = sort_recent, wrap = true }
+  )
+end, { desc = "Next recent visited path" })
+
+vim.keymap.set("n", "[s", function()
+  visits.iterate_paths(
+    "backward",
+    vim.fn.getcwd(),
+    { sort = sort_recent, wrap = true }
+  )
+end, { desc = "Previous recent visited path" })
