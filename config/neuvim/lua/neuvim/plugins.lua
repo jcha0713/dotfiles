2d33c542787d2603495e2c9825e90bfae6d329a0
jcha0713 <joocha0713@gmail.com>
2026-04-18 12:04:47 +0900

    nvim: categorize plugins with comments

--- config/neuvim/lua/neuvim/plugins.lua
+++ config/neuvim/lua/neuvim/plugins.lua
@@ -8,24 +8,27 @@ return {
   "e-ink-colorscheme/e-ink.nvim",
   "kungfusheep/mfd.nvim",
 
+  -- UI
+  { "MeanderingProgrammer/render-markdown.nvim", event = "BufEnter" },
+
+  -- Navigation
   "mikavilpas/yazi.nvim",
   "comfysage/artio.nvim",
+  { "bassamsdata/namu.nvim", event = "BufEnter" },
+  { "yorickpeterse/nvim-jump", name = "jump", event = "BufEnter" },
 
+  -- Mini
   "nvim-mini/mini.icons",
   { "nvim-mini/mini.pairs", event = "BufRead" },
   { "nvim-mini/mini.diff", event = "BufEnter" },
   { "nvim-mini/mini.visits", event = "BufEnter" },
   { "nvim-mini/mini.surround", event = "BufEnter" },
 
-  { "MeanderingProgrammer/render-markdown.nvim", event = "BufEnter" },
-  { "bassamsdata/namu.nvim", event = "BufEnter" },
-
+  -- Format/Lint
   "stevearc/conform.nvim",
   { "mfussenegger/nvim-lint", name = "lint" },
 
+  -- Completion
   { "saghen/blink.cmp", name = "blink", version = vim.version.range("^1") },
-
   { "cursortab/cursortab.nvim", name = "cursortab" },
-
-  { "yorickpeterse/nvim-jump", name = "jump", event = "BufEnter" },
 }
