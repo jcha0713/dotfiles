871000445948f2e13cd3143f16336134f5b716ea
jcha0713 <joocha0713@gmail.com>
2026-04-29 15:04:14 +0900

    nvim: add keymaps for quickfix

--- config/neuvim/lua/neuvim/keymaps.lua
+++ config/neuvim/lua/neuvim/keymaps.lua
@@ -2,7 +2,22 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "vv", "<S-v>", { desc = "Select line" })
 
-vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", { desc = "Quit nvim" })
+vim.keymap.set("n", "<leader><leader>q", "<cmd>qa<cr>", { desc = "Quit nvim" })
+vim.keymap.set(
+  "n",
+  "<leader>qo",
+  "<cmd>botright cwindow<cr>",
+  { desc = "Open quickfix" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>qq",
+  "<cmd>cclose<cr>",
+  { desc = "Close quickfix" }
+)
+vim.keymap.set("n", "<leader>qd", function()
+  vim.diagnostic.setqflist({ title = "Diagnostics" })
+end, { desc = "Send diagnostics to quickfix" })
 
 vim.keymap.set("n", "gh", "^", { desc = "Move to first character in line" })
 vim.keymap.set("n", "gl", "$", { desc = "Move to last character in line" })
