52729740eff481f335314ddb3af785deae146f91
jcha0713 <joocha0713@gmail.com>
2026-04-30 16:34:33 +0900

    nvim: add more keymaps for namu

--- config/neuvim/config/namu.lua
+++ config/neuvim/config/namu.lua
@@ -1,6 +1,29 @@
+require("namu").setup({
+  namu_symbols = {
+    enable = true,
+    options = {
+      row_position = "top10_right",
+    },
+  },
+})
+
 vim.keymap.set(
   "n",
   "<leader>nc",
   ":Namu call both<cr>",
   { desc = "Namu call both" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>nt",
+  ":Namu treesitter<cr>",
+  { desc = "Namu treesitter" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ns",
+  ":Namu symbols<cr>",
+  { desc = "Namu symbols" }
 )
